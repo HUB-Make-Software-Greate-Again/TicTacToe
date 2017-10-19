2323559c4f8306fb2b893121787938493c2fcb9f
Gunnar Baldursson <Gunso_Baldurs@hotmail.com>
2017-10-19 18:26:23 +0000

    Refactored to clean up the code a bit in TTT class

--- src/main/java/is/ru/hugb/TicTacToe.java
+++ src/main/java/is/ru/hugb/TicTacToe.java
@@ -8,58 +8,25 @@ public class TicTacToe{
     private int[][] grid;
     private int player;
     private int winner;
+    private boolean gameover;
 
     public TicTacToe(){
         this.grid = new int[3][3];
         
         this.player = this.X;
         this.winner = this.EMPTY;
+        this.gameover = false;
     }
 
     public boolean gameOver(){
-        boolean gameover = false;
-
-        // Check row winner
-        for (int i = 0; i < 3; ++i){
-            if (this.grid[i][0] != this.EMPTY){
-                if (this.grid[i][0] == this.grid[i][1] && this.grid[i][0] == this.grid[i][2]){
-                    gameover = true;
-                    this.winner = this.grid[i][0];
-                }
-            }
-        }
-        
-        // Check column winner
-        for (int i = 0; i < 3; ++i){
-            if (this.grid[0][i] != this.EMPTY){
-                if (this.grid[0][i] == this.grid[1][i] && this.grid[0][i] == this.grid[2][i]){
-                    gameover = true;
-                    this.winner = this.grid[0][i];
-                }
-            }
-        }
-
-        // Check left dia
-        if (this.grid[0][0] != this.EMPTY){
-            if (this.grid[0][0] == this.grid[1][1] && this.grid[0][0] == this.grid[2][2]){
-                gameover = true;
-                this.winner = this.grid[0][0];
-            }
-        }
-
-        // Check right dia
-        if (this.grid[2][0] != this.EMPTY){
-            if (this.grid[0][2] == this.grid[1][1] && this.grid[0][2] == this.grid[2][0]){
-                gameover = true;
-                this.winner = this.grid[0][2];
-            }
-        }
-
-        return gameover;
+        return this.gameover;
     }
 
     public void doMove(int x, int y){
         this.grid[x][y] = this.player;
+
+        this.checkGameOver();
+
         if (this.player == this.X){
             this.player = this.O;
         } else {
@@ -69,5 +36,51 @@ public class TicTacToe{
 
     public int winner(){
         return this.winner;
+    }
+
+    private void checkGameOver(){
+        this.checkRows();        
+        this.checkCols();
+        this.checkDias();
+    }
+
+    private void checkDias(){
+        // Check left diagonal
+        if (this.grid[0][0] == this.player){
+            if (this.grid[0][0] == this.grid[1][1] && this.grid[0][0] == this.grid[2][2]){
+                this.gameover = true;
+                this.winner = this.player;
+            }
+        }
+
+        // Check right diagonal
+        if (this.grid[2][0] == this.player){
+            if (this.grid[0][2] == this.grid[1][1] && this.grid[0][2] == this.grid[2][0]){
+                this.gameover = true;
+                this.winner = this.player;
+            }
+        }
+    }
+
+    private void checkRows(){
+        for (int i = 0; i < 3; ++i){
+            if (this.grid[i][0] == this.player){
+                if (this.grid[i][0] == this.grid[i][1] && this.grid[i][0] == this.grid[i][2]){
+                    this.gameover = true;
+                    this.winner = this.player;
+                }
+            }
+        }
+    }
+
+    private void checkCols(){
+        for (int i = 0; i < 3; ++i){
+            if (this.grid[0][i] == this.player){
+                if (this.grid[0][i] == this.grid[1][i] && this.grid[0][i] == this.grid[2][i]){
+                    this.gameover = true;
+                    this.winner = this.player;
+                }
+            }
+        }
     }
 }
