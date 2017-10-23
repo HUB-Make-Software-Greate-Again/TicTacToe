d696be63292c0a33aed0b11b6292b872689a4010
Gunnar Baldursson <Gunso_Baldurs@hotmail.com>
2017-10-23 20:51:55 +0000

    Declared post routes in app.java

--- src/main/java/is/ru/hugb/App.java
+++ src/main/java/is/ru/hugb/App.java
@@ -1,19 +1,42 @@
 import static spark.Spark.*;
+import is.ru.hugb.TicTacToe;
 
 public class App
 {
-    private static final String SESSION_ID = "game_id";
+    private static final String GAME = "tictactoe_instance";
 
     public static void main(String[] args){
-        staticFileLocation("/public");
+        staticFiles.location("/public");
+        
+        post("/game", (req, res) -> {
+            // If request doesn't have x & y params, return error
+            if (req.session().attribute(GAME) == null){
+                req.session().attribute(GAME, new TicTacToe());
+            }
 
-        get("/", (req, res) -> {
-            req.session().attribute(SESSION_ID, "foo");
-            return res;
+            int x = Integer.parseInt(req.queryParams("x"));
+            int y = Integer.parseInt(req.queryParams("y"));
+
+            TicTacToe game = req.session().attribute(GAME);
+
+            // try
+            game.doMove(x, y);
+            // catch, return illegal move response
+
+            // Return gameover respone
+            if (game.gameOver()){
+                return "done";
+            } 
+
+            // Return everything's ok response
+            return "foo";
         });
 
-        get("/game", (req, res) -> {
-            return req.session().attribute(SESSION_ID);
+        post("/reset", (req, res) -> {
+            req.session().attribute(GAME, new TicTacToe());
+
+            res.redirect("/");
+            return res;
         });
     }
 }
