d1f1275766627744dc15f5ceb3e00c44f964fda8
Sæþór Hallgrímsson <saethor@hringdu.is>
2017-10-26 17:20:34 +0000

    Tested clicking for both x and y

--- src/selenium/java/is/ru/hugb/AppTest.java
+++ src/selenium/java/is/ru/hugb/AppTest.java
@@ -11,6 +11,35 @@ public class AppTest extends SeleniumTestWrapper {
   @Test
   public void testTitleMatches() {
     driver.get(baseUrl);
-    assertEquals("Document", driver.getTitle());
+    assertEquals("Tic Tac Toe", driver.getTitle());
+  }
+
+  // Leikur endar
+  // Jafntefli
+  // Ýta á sama reit 2x
+  // sé hægt að ýta á alla reiti
+
+  @Test
+  public void testDoFirstMove() throws Exception {
+    driver.get(baseUrl);
+    WebElement el = driver.findElement(By.id("one"));
+    el.click();
+    assertEquals("X", el.getText());
+  }
+
+  @Test
+  public void testDoSecondMove() throws Exception {
+    driver.get(baseUrl);
+    WebElement el = driver.findElement(By.id("two"));
+    el.click();
+    assertEquals("O", el.getText());
+  }
+
+  @Test
+  public void testDoSecondMove() throws Exception {
+    driver.get(baseUrl);
+    WebElement el = driver.findElement(By.id("two"));
+    el.click();
+    assertEquals("O", el.getText());
   }
 }
